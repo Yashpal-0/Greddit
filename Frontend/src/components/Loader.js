73d5d76ad4e2ad51ec42d100a0cd5a6ee587c4ca
Yashpal-0 <yashpalyadav050@gmail.com>
2024-04-23 22:11:39 +0530

    update

--- Frontend/src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Loader() {
-    return (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "flexDirection",flexDirection:'row', backgroundColor: "black", height: "200vh" ,width:"300vh" }}>
-            
-            <img alt='' src="https://i.pinimg.com/originals/f5/84/dc/f584dc5c28b1910b183e344d97d1feaa.gif" style={{"height": '20vh',"width":'20vh' , "marginRight":'120vh',"marginTop":'45vh'}}></img>
-        </div>
-    )
-}
